a0f8973d5649d084226ba2ebaa9d9a1c3cf6a449
ShevchenkoW3 <shevdan03@yandex.ru>
2024-03-22 18:30:21 +0300

    Updating index page

---
+++ template/footer.php
@@ -0,0 +1,25 @@
+<footer class="podval">
+<nav class="navbar navbar-expand-lg text-white" style="background-color:#000000;">
+<div class="container">
+             <!-- <div class="col-1"></div> -->
+             <div class="copyright">                    
+                <!-- <div class ="copy"> -->
+                <p>
+                Copyright <?php echo '&copy2024'; ?>
+                <!-- </p> -->
+            </div>
+                <div class="social">
+                    
+
+                </div>
+                <div class="contacts">
+                    <img src="img/location.png" class="d-inline-block align-text-top" alt="location"><p>Контакты: <br> г. Москва</p>
+                    <img src="img/phone.png" class="d-inline-block align-text-top" alt="phone"><br><a class="phone" href="#"><i class="fa fa-phone"></i>+790359444</a>&nbsp;&nbsp; 
+                </div>
+             <!-- <div class="col-2"></div>    
+             <div class="col-1"></div> -->
+        </div>
+</nav>
+    </footer>
+</body>
+</html>
